cab27804091737cda928935eb3a42bbf61902cef
onursolmaz <solmaz_onur@hotmail.com>
2021-07-02 21:18:38 +0300

    refactoring

--- src/com/company/week1/LetterGrade.java
+++ src/com/company/week1/LetterGrade.java
@@ -11,21 +11,21 @@ public class LetterGrade {
         number = input.nextDouble();
 
         if(number >0 && number <= 100) {
-            if (number >= 90) {
+            if (number >= 90)
                 System.out.println("AA");
-            } else if (number >= 80) {
+             else if (number >= 80)
                 System.out.println("BA");
-            } else if (number >= 70) {
+             else if (number >= 70)
                 System.out.println("BB+");
-            } else if (number >= 60) {
+             else if (number >= 60)
                 System.out.println("BB");
-            } else if (number >= 50) {
+             else if (number >= 50)
                 System.out.println("BC");
-            } else if (number >= 45) {
+             else if (number >= 45)
                 System.out.println("CC");
-            } else if (number < 45) {
+             else
                 System.out.println("FF");
-            }
+
         }else
             System.out.println("Please enter valid score");
 
